d207407baee054100e2ef252e3ebbab6f231255b
MiguelHenriqueO <miguelhenriqueisaac@gmail.com>
2025-09-05 20:42:29 -0300

    primeira aula python

---
+++ Python/aula1.py
@@ -0,0 +1,43 @@
+#x = 10
+
+#print("x vale:",x)
+
+#print(type (x))
+
+#y = 33.3
+#print(type(y))
+
+#a = "facef"
+#print(type(a))
+
+#idade = 25
+#altura = 1.76
+#print("A altura é", altura, "e a idade é", idade)
+#print(f"A altura é {altura} e a idade é {idade}")
+
+#nome = input("digite seu nome: \n")
+#print(f"seu nome é: {nome}")
+
+#num = input("digite um número: ")
+#print("seu número é:", num )
+#print(type(num))
+
+#a = 2.33
+#print(str(a))
+
+#b= '333'
+#print(b)
+
+#c= int(b)+2
+#print(c)
+
+#num = 7/3
+
+#print("numero", num)
+#print(type(num))
+#print("numero", round(num))
+#print(type(round(num)))
+
+n= int(input("digite um valor inteiro:"))
+n=n*2
+print("o dobro do valor de n é:", n)
